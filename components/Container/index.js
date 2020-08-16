c743ab158be8a0a5c83a20bcd59dcc0fe8ff9c10
Adib Khan <adib.khan@gmail.com>
2020-08-16 18:00:52 -0400

    feat: add Container

---
+++ components/Container/index.js
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+
+function Container ({ children }) {
+  return <section className="contain">
+    { children }
+  </section>
+}
+
+Container.propTypes = {
+  children: PropTypes.any,
+}
+
+export default Container;
