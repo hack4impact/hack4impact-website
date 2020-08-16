7821a7a3cce536a370403ec3398c98bc60371861
Adib Khan <adib.khan@gmail.com>
2020-08-16 01:27:57 -0400

    feat: add Nav

---
+++ components/Nav/index.js
@@ -0,0 +1,35 @@
+import styles from './styles.module.scss';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+function NavLink({children, href, activeRoute}) {
+  return (
+    <li className={activeRoute === href ? styles.activeRoute : ''}>
+      <Link href={href}>
+        <a>{children}</a>
+      </Link>
+    </li>
+  )
+}
+ 
+function Nav() {
+  const router = useRouter()
+  const activeRoute = router.pathname
+
+  return(
+    <header>
+      <nav className={styles.navContainer}>
+        <ul>
+          <NavLink activeRoute={activeRoute} href="/">
+            <div className={styles.image}></div>
+          </NavLink>
+          <NavLink activeRoute={activeRoute} href="/about">About Us</NavLink>
+          <NavLink activeRoute={activeRoute} href="/work">Our Work</NavLink>
+          <NavLink activeRoute={activeRoute} href="/apply">Apply</NavLink>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Nav;
