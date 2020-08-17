d4e3ce9d3d50ec6fafceeaa6aad3675e967f6cc5
Adib Khan <adib.khan@gmail.com>
2020-08-17 17:32:07 -0400

    feat: add ImageHighlight

---
+++ components/ImageHighlight/index.js
@@ -0,0 +1,28 @@
+import PropType from 'prop-types';
+import DotShadow from '../DotShadow';
+import Accent from '../Accent';
+import styles from './styles.module.scss';
+
+function ImageHighlight({ children, height, width }) {
+  console.log(height);
+  return (
+    <div className={styles.root}>
+      <div className={styles.dot_shadow}>
+        <DotShadow height={height + 30} width={width + 30} />
+      </div>
+      {children}
+      <div className={styles.accent_wrapper} style={{ height: height + 20, width: width + 20 }}>
+        <Accent className={styles.accent_1} />
+        <Accent className={styles.accent_2} />
+      </div>
+    </div>
+  );
+}
+
+ImageHighlight.propTypes = {
+  children: PropType.element,
+  height: PropType.number,
+  width: PropType.number,
+};
+
+export default ImageHighlight;
