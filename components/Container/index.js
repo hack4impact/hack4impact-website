80186aecd34410f76c6ebd05849f58b1c9c522fd
Adib Khan <adib.khan@gmail.com>
2020-08-17 16:13:19 -0400

    feat: add spacer to container

--- components/Container/index.js
+++ components/Container/index.js
@@ -1,8 +1,14 @@
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import Spacer from '../Spacer';
 
 function Container({ children, className }) {
-  return <section className={classNames('contain', className)}>{children}</section>;
+  return (
+    <>
+      <section className={classNames('contain', className)}>{children}</section>
+      <Spacer />
+    </>
+  );
 }
 
 Container.propTypes = {
