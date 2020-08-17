e338689fb672571442cd90bac1f7aa6a0037bb9c
Adib Khan <adib.khan@gmail.com>
2020-08-17 01:30:40 -0400

    feat: upgrade apply header

--- pages/apply/index.js
+++ pages/apply/index.js
@@ -1,9 +1,15 @@
 import Header from '../../components/Header';
+import styles from './styles.module.scss';
 
 function Apply() {
   return (
     <main>
-      <Header title="About" />
+      <Header title="Apply">
+        <div className={styles.switch_control}>
+          <a>For New Chapters</a>
+          <a>For Non Profits</a>
+        </div>
+      </Header>
     </main>
   );
 }
