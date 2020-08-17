cbe4f8dafe5e48bc3bae55f6470b4e9753f9b233
Adib Khan <adib.khan@gmail.com>
2020-08-17 15:28:31 -0400

    feat: add className prop to Container

--- components/Container/index.js
+++ components/Container/index.js
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
-function Container({ children }) {
-  return <section className="contain">{children}</section>;
+function Container({ children, className }) {
+  return <section className={classNames('contain', className)}>{children}</section>;
 }
 
 Container.propTypes = {
   children: PropTypes.any,
+  className: PropTypes.string,
 };
 
 export default Container;
