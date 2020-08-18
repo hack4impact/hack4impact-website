f81bef2c650ff2772557084d573c07384a8e5189
Adib Khan <adib.khan@gmail.com>
2020-08-17 21:12:20 -0400

    feat: add requirements content

--- pages/apply/index.js
+++ pages/apply/index.js
@@ -1,14 +1,51 @@
 import { useRouter } from 'next/router';
 import Header from '../../components/Header';
+import ImageHighlight from '../../components/ImageHighlight';
+import Container from '../../components/Container';
+import Button from '../../components/Button';
 import styles from './styles.module.scss';
 import classNames from 'classnames';
 
 const npRoute = '/apply/nonprofit';
 const chRoute = '/apply/chapter';
 
+const height = 200;
+const width = 260;
+
+const pretendImContentful1 = {
+  requirements:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel odio erat. Nullam vel risus nisi. Etiam sed nisi augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi a odio rutrum, feugiat elit quis, aliquam enim. Aliquam vel vehicula nisi, in venenatis nulla. Maecenas mollis in metus in pulvinar. Nullam consequat massa quis quam tristique, sit amet pulvinar lectus pharetra. Donec ornare purus at ultrices facilisis.  ',
+  image: 'http://placekitten.com/g/1000/600',
+  requirementLink: 'http://placekitten.com',
+  faqTitle: 'Chapters FAQ',
+  faq: [
+    {
+      question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
+      answer:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel odio erat. Nullam vel risus nisi. Etiam sed nisi augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi a odio rutrum, feugiat elit quis, aliquam enim. Aliquam vel vehicula nisi, in venenatis nulla. Maecenas mollis in metus in pulvinar. Nullam consequat massa quis quam tristique, sit amet pulvinar lectus pharetra. Donec ornare purus at ultrices facilisis.',
+    },
+  ],
+};
+
+const pretendImContentful2 = {
+  requirements:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel odio erat. Nullam vel risus nisi. Etiam sed nisi augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi a odio rutrum, feugiat elit quis, aliquam enim. Aliquam vel vehicula nisi, in venenatis nulla. Maecenas mollis in metus in pulvinar. Nullam consequat massa quis quam tristique, sit amet pulvinar lectus pharetra. Donec ornare purus at ultrices facilisis.  ',
+  image: 'http://placekitten.com/g/1000/600',
+  requirementLink: 'http://placekitten.com',
+  faqTitle: 'Nonprofits FAQ',
+  faq: [
+    {
+      question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
+      answer:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel odio erat. Nullam vel risus nisi. Etiam sed nisi augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi a odio rutrum, feugiat elit quis, aliquam enim. Aliquam vel vehicula nisi, in venenatis nulla. Maecenas mollis in metus in pulvinar. Nullam consequat massa quis quam tristique, sit amet pulvinar lectus pharetra. Donec ornare purus at ultrices facilisis.',
+    },
+  ],
+};
+
 function Apply() {
   const router = useRouter();
   const currentRoute = router.pathname;
+  const content = currentRoute === chRoute ? pretendImContentful1 : pretendImContentful2;
   return (
     <main>
       <Header title="Apply">
@@ -21,6 +58,25 @@ function Apply() {
           </a>
         </div>
       </Header>
+      <div style={{ marginTop: 20 }}>
+        <Container className="row spaced aligned wrap">
+          <div className={styles.requirements}>
+            <h2>{"What you'll need"}</h2>
+            <p style={{ marginBottom: 40 }}>{content.requirements}</p>
+            <Button href={content.requirementLink} external>
+              Application Here
+            </Button>
+          </div>
+          <div className={styles.image_wrap}>
+            <ImageHighlight height={height} width={width}>
+              <img src={content.image} alt="" style={{ height, width }} className={styles.image} />
+            </ImageHighlight>
+          </div>
+        </Container>
+        <Container>
+          <h2>{content.faqTitle}</h2>
+        </Container>
+      </div>
     </main>
   );
 }
