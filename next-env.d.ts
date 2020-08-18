d3d7fa5ed5d4bd2f7260d4c6b7bf6ccf599b97d7
Adib Khan <adib.khan@gmail.com>
2020-08-18 15:58:55 -0400

    fix: upgrade scss interaction

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,3 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-declare module '*.scss';
+declare module '*.scss' {
+  export const style: { [className: string]: string };
+  export default style;
+}
