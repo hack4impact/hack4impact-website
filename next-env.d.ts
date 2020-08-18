63103933136ee0b0cfd925759117620abd4d24e2
Adib Khan <adib.khan@gmail.com>
2020-08-18 16:01:45 -0400

    fix: fixed naming

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 declare module '*.scss' {
-  export const style: { [className: string]: string };
-  export default style;
+  export const styles: { [className: string]: string };
+  export default styles;
 }
