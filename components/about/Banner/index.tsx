6456895ae08dfb062858b2e7577bfa20cd7ecc9b
Holben888 <holben888@gmail.com>
2020-10-01 13:49:51 -0400

    feat: add animation to mission, exec members

--- components/about/Banner/index.tsx
+++ components/about/Banner/index.tsx
@@ -1,5 +1,6 @@
 import styles from './styles.module.scss';
 import Container from '@components/shared/Container';
+import { motion } from 'framer-motion';
 
 function Banner() {
   return (
@@ -17,11 +18,28 @@ function Banner() {
       </div>
       <Container>
         <h2>Our Mission</h2>
-        <div className={styles.mission}>
+        <motion.div
+          className={styles.mission}
+          initial="hidden"
+          animate="show"
+          variants={{
+            hidden: { opacity: 0, scale: 0 },
+            show: {
+              opacity: 1,
+              scale: 1,
+              transition: {
+                type: 'spring',
+                bounce: 0.4,
+                duration: 0.6,
+              },
+            },
+          }}>
           <p>
-          To empower engineers, designers, activists, and humanitarians to create <strong>lasting and impactful social change</strong>, fostering the wider adoption of software as a tool for social good.
+            To empower engineers, designers, activists, and humanitarians to create{' '}
+            <strong>lasting and impactful social change</strong>, fostering the wider adoption of
+            software as a tool for social good.
           </p>
-        </div>
+        </motion.div>
       </Container>
     </>
   );
--- components/shared/StaggerAnimOnScroll/index.tsx
+++ components/shared/StaggerAnimOnScroll/index.tsx
@@ -3,29 +3,29 @@ import { motion, Variant } from 'framer-motion';
 import { ReactNode } from 'react';
 
 type StaggerItemProps = {
-  show?: Variant;
-  hidden?: Variant;
+  animShow?: Variant;
+  animHidden?: Variant;
   children: ReactNode;
 };
 
-export function StaggerItem({ children, show, hidden }: StaggerItemProps) {
+export function StaggerItem({ children, animShow, animHidden }: StaggerItemProps) {
   return (
     <motion.div
       initial="hidden"
       variants={{
-        hidden: hidden || { opacity: 0, x: -300 },
-        show: show || { opacity: 1, x: 0 },
+        hidden: animHidden || { opacity: 0, x: -300 },
+        show: animShow || { opacity: 1, x: 0 },
       }}>
       {children}
     </motion.div>
   );
 }
 
-type StaggerAnimOnScrollProps = {
+type StaggerAnimOnScrollProps = React.HTMLAttributes<HTMLDivElement> & {
   children: ReactNode;
 };
 
-function StaggerAnimOnScroll({ children }: StaggerAnimOnScrollProps) {
+function StaggerAnimOnScroll({ children, className }: StaggerAnimOnScrollProps) {
   const [ref, inView] = useInView();
 
   return (
@@ -33,13 +33,13 @@ function StaggerAnimOnScroll({ children }: StaggerAnimOnScrollProps) {
       animate={inView ? 'show' : 'hidden'}
       initial="hidden"
       exit="hidden"
+      className={className}
       variants={{
         hidden: {
-          transition: {
-            staggerChildren: 0.1,
-          },
+          opacity: 0,
         },
         show: {
+          opacity: 1,
           transition: {
             staggerChildren: 0.1,
           },
